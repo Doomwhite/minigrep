486e7321cc43cfa7d2312a51e209242f7deef43e
Doomwhite <doomwhitex@gmail.com>
2023-01-28 22:06:06 -0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use minigrep::Config;
+
+use std::env;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config: Config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(0);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_name);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(0)
+    }
+}
